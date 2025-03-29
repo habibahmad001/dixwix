77c99d27d0dd5b48e69078398c35c7a6c931b79d
Habib Ahmed <habibahmed001@gmail.com>
2025-03-29 21:48:57 +0500

    Remove Archive file

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -282,25 +282,6 @@ class RewardController extends Controller
                 ]);
             }
 
-            /******** Send Request To Gifto *******/
-            $giftoGramResponse = "";
-            if($request->is_gifto == 1) {
-                $giftoGramResponse = app(GiftoGramService::class)->sendGift(
-                    $user->email,
-                    $request->gifto_price,
-                    $request->gifto_msg,
-                    $request->comp
-                );
-
-                if(!$giftoGramResponse) {
-                    return response()->json([
-                        'success' => false,
-                        'message' => 'To send card gifto amount must be multiple of 5.',
-                    ]);
-                }
-            }
-            /******** Send Request To Gifto *******/
-
             TransferRequest::create([
                 'from_user_id' => $authUser->id,
                 'to_user_id' => $user->id,
@@ -313,6 +294,17 @@ class RewardController extends Controller
             ]);
         }
 
+        /******** Send Request To Gifto *******/
+        $giftoGramResponse = "";
+        if($request->is_gifto == 1) {
+            $giftoGramResponse = app(GiftoGramService::class)->sendGift(
+                $user->email,
+                $request->gifto_price,
+                $request->gifto_msg,
+                $request->comp
+            );
+        }
+        /******** Send Request To Gifto *******/
         // Directly transfer points if within limit
         $this->processPointTransfer($authUser, $user, $request->points);
 
